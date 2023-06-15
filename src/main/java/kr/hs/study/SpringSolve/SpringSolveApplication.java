9c738ccbf00eaeadc3af8a18861c3346f044b1d5
Juwan Lee <ljw05w5@gmail.com>
2023-06-15 14:42:29 +0900

    Initial commit

---
+++ src/main/java/kr/hs/study/SpringSolve/SpringSolveApplication.java
@@ -0,0 +1,13 @@
+package kr.hs.study.SpringSolve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSolveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSolveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/hs/study/SpringSolve/SpringSolveApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hs.study.SpringSolve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSolveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
